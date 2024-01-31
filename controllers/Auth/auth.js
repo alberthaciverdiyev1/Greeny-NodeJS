c2815c71d513a9e47dc2b769358eecefd14f9b2f
alberthaciverdiyev1 <alberthaciverdiyev55@gmail.com>
2024-01-31 12:18:16 -0800

    uncompleted correctly login

--- controllers/Auth/auth.js
+++ controllers/Auth/auth.js
@@ -3,6 +3,8 @@ const jwt = require('jsonwebtoken');
 const User = require('../../Models/Auth');
 const bcrypt = require('bcrypt');
 
+
+
 const Register = (req, res) => {
   res.render('Auth/register', { css: 'user-auth.css' });
 };
@@ -14,9 +16,9 @@ const LoginIndex = (req, res) => {
 
 const RegisterUser = async (req, res) => {
   try {
-    const { name, surname,email, age, password } = req.body;
+    const { name, surname, email, age, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new User({ name, surname,email, age, password: hashedPassword });
+    const newUser = new User({ name, surname, email, age, password: hashedPassword });
     await newUser.save();
     res.status(201).json('User created successfully');
   } catch (err) {
@@ -26,52 +28,49 @@ const RegisterUser = async (req, res) => {
 
 module.exports = RegisterUser;
 
-// const Login = async (req, res) => {
-//   const email = req.body.email;
-//   const password = req.body.password;
-//   const foundUser = await user.findOne({ email: email });
-//   // const foundUser = await user.findOne({ email: email });
-
-//   console.log(email);
-//   console.log(foundUser);
-
-//   if (foundUser) {
-//     if (foundUser.password === password) {
-//       res.json({ status: 200, message: 'Logged in' });
-//     } else {
-//       res.json({ status: 404, message: 'Wrong password' });
-//     }
-//   } else {
-//     res.json({ status: 404, message: 'User not found' });
-//   }
-
-// };
-
 const Login = async (req, res) => {
   try {
     const { email, password } = req.body;
     const foundUser = await User.findOne({ email: email });
-    if (foundUser) {
-      const hashedpassword = await bcrypt.compare(password, foundUser.password);
-      if (hashedpassword) {
+    req.session = {};
 
-        const token = jwt.sign({ userId: User._id }, 'your-secret-key', {
+    if (foundUser) {
+      const hashedPassword = await bcrypt.compare(password, foundUser.password);
+
+      if (hashedPassword) {
+        // const token = jwt.sign({ userId: foundUser._id }, process.env.JWT_SECRET, {
+        //   expiresIn: '1h',
+        // });
+
+        const token = jwt.sign({ userId: foundUser._id }, 'my-secret-key-is-2001', {
           expiresIn: '1h',
         });
-        res.status(200).json({ token });
+
+        req.session.user = {
+          userId: foundUser._id,
+          email: foundUser.email,
+          name: foundUser.name,
+          surname: foundUser.surname,
+          gender: foundUser.gender,
+        };
+
+        res.locals.user = req.session.user || null;
+
       } else {
         res.status(404).json({ message: 'Wrong password' });
       }
-    }else{
-      res.status(404).json({ message: 'Wrong password 0r Email' });
+    } else {
+      res.locals.user = null;
+      res.status(404).json({ message: 'Wrong email or password' });
     }
   } catch (error) {
-    console.log(error)
-    res.status(400).json({ message: error });
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
   }
 };
+
 const LogOut = (req, res) => {
-  req.Session.destroy();
+  req.session.destroy();
   res.redirect('/');
 };
 module.exports = {
